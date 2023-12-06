6bdc00fccdf218b7da028ed9c114ea47b22a0aa3
zehrasbr <zehraisbr@gmail.com>
2023-12-06 19:15:42 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,13 @@ public class Main {
 
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde switch case kullanınız.
+        //90 ve üzeri AA , 80-90 arası BA , 70-79 arası BB, 60-69 arası CB , 50-59 arası CC , 50 den kücük FF
         NotHesaplama notHesaplama = new NotHesaplama();
         notHesaplama.NotHesaplama();
 
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde if else  kullanınız.
+        //90 ve üzeri AA , 80-90 arası BA , 70-79 arası BB, 60-69 arası CB , 50-59 arası CC , 50 den kücük FF
         NotHesapIfElse notHesapIfElse = new NotHesapIfElse();
         notHesapIfElse.NotHesapIfElse();
     }
