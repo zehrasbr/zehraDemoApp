4caf0c11f86de30e21ba6306491db7a10d2d3933
zehrasbr <zehraisbr@gmail.com>
2023-12-06 18:59:21 +0300

    update

--- src/HesapMakinesi.java
+++ src/HesapMakinesi.java
@@ -3,9 +3,10 @@ public class HesapMakinesi {
     {
     for(int i=1;i<=10;i++)
     {
-        for(int j=1; j<=10;i++)
+        System.out.println("");
+        for(int j=1; j<=10;j++)
         {
-
+            System.out.println( i+" x "+ j+ " = "+(j*i));
         }
     }
     }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    Yazdir yazdir = new Yazdir();
-    yazdir.yazdir();
+    /*Yazdir yazdir = new Yazdir();
+    yazdir.yazdir();*/
 
     HesapMakinesi hesapMakinesi = new HesapMakinesi();
     hesapMakinesi.HesapMakinesi();
