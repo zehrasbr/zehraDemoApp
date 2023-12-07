fc464c01c78db295a2e93c5b4f91f43336a2946d
zehrasbr <zehraisbr@gmail.com>
2023-12-07 19:50:16 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     yazdir.yazdir();*/
 
         //Çarpım tablosunu oluşturan bir sınıf yazın.
-        HesapMakinesi hesapMakinesi = new HesapMakinesi();
-        hesapMakinesi.HesapMakinesi();
+        //HesapMakinesi hesapMakinesi = new HesapMakinesi();
+        //hesapMakinesi.HesapMakinesi();
 
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde switch case kullanınız.
@@ -19,8 +19,8 @@ public class Main {
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde if else  kullanınız.
         //90 ve üzeri AA , 80-90 arası BA , 70-79 arası BB, 60-69 arası CB , 50-59 arası CC , 50 den kücük FF
-        NotHesapIfElse notHesapIfElse = new NotHesapIfElse();
-        notHesapIfElse.NotHesapIfElse();
+        //NotHesapIfElse notHesapIfElse = new NotHesapIfElse();
+        //notHesapIfElse.NotHesapIfElse();
     }
 }
 
--- src/NotHesaplama.java
+++ src/NotHesaplama.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class NotHesaplama {
     public void NotHesaplama(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Not gir: ");
+        int not = scanner.nextInt();
 
+        switch (not/10)
+        {
+                case 9:
+                System.out.println( "AA");
+                break;
+            case 8:
+                System.out.println( "BA");
+                break;
+            case 7:
+                System.out.println( "BB");
+                break;
+        }
     }
 }
