5c1071dc874a1d89ad71098b104e04f24a48c46d
zehrasbr <zehraisbr@gmail.com>
2023-12-07 20:12:19 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    /*Yazdir yazdir = new Yazdir();
-    yazdir.yazdir();*/
+        //Yazdir yazdir = new Yazdir();
+        //yazdir.yazdir();
 
         //Çarpım tablosunu oluşturan bir sınıf yazın.
         //HesapMakinesi hesapMakinesi = new HesapMakinesi();
@@ -13,14 +13,14 @@ public class Main {
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde switch case kullanınız.
         //90 ve üzeri AA , 80-90 arası BA , 70-79 arası BB, 60-69 arası CB , 50-59 arası CC , 50 den kücük FF
-        NotHesaplama notHesaplama = new NotHesaplama();
-        notHesaplama.NotHesaplama();
+        //NotHesaplama notHesaplama = new NotHesaplama();
+        //notHesaplama.NotHesaplama();
 
         //100 lük sayı sisteminde girilen notları sayı sistemine çeviren kodu yazınız.
         // Bu ödevde if else  kullanınız.
         //90 ve üzeri AA , 80-90 arası BA , 70-79 arası BB, 60-69 arası CB , 50-59 arası CC , 50 den kücük FF
-        //NotHesapIfElse notHesapIfElse = new NotHesapIfElse();
-        //notHesapIfElse.NotHesapIfElse();
+        NotHesapIfElse notHesapIfElse = new NotHesapIfElse();
+        notHesapIfElse.NotHesapIfElse();
     }
 }
 
--- src/NotHesapIfElse.java
+++ src/NotHesapIfElse.java
@@ -1,6 +1,23 @@
-public class NotHesapIfElse {
-    public void NotHesapIfElse()
-    {
+import java.util.Scanner;
 
+public class NotHesapIfElse {
+    public void NotHesapIfElse() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Not gir: ");
+        int not = scanner.nextInt();
+
+        if (not >= 90) {
+            System.out.println("AA");
+        } else if (not >= 80) {
+            System.out.println("BA");
+        } else if (not >= 70) {
+            System.out.println("BB");
+        } else if (not >= 60) {
+            System.out.println("CB");
+        } else if (not >= 50) {
+            System.out.println("CC");
+        } else {
+            System.out.println("FF");
+        }
     }
 }
