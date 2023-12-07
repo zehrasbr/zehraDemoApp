6c41b5e609b330610651ed3a64ad5e8a202d7388
zehrasbr <zehraisbr@gmail.com>
2023-12-07 19:57:21 +0300

    update

--- src/NotHesaplama.java
+++ src/NotHesaplama.java
@@ -8,14 +8,35 @@ public class NotHesaplama {
 
         switch (not/10)
         {
+            case 10:
+                System.out.println( "Ders Notu: AA");
+                break;
                 case 9:
-                System.out.println( "AA");
+                System.out.println( "Ders Notu: AA");
                 break;
             case 8:
-                System.out.println( "BA");
+                System.out.println( "Ders Notu: BA");
                 break;
             case 7:
-                System.out.println( "BB");
+                System.out.println( "Ders Notu: BB");
+                break;
+            case 6:
+                System.out.println( "Ders Notu: CB");
+                break;
+            case 5:
+                System.out.println( "Ders Notu: CC");
+                break;
+            case 4:
+                System.out.println( "Ders Notu: FF");
+                break;
+            case 3:
+                System.out.println( "Ders Notu: FF");
+                break;
+            case 2:
+                System.out.println( "Ders Notu: FF");
+                break;
+            case 1:
+                System.out.println( "Ders Notu: FF");
                 break;
         }
     }
