a84f19a36dd57413480f0fe2df91762906887884
zehrasbr <zehraisbr@gmail.com>
2023-12-08 12:46:41 +0300

    update

--- src/Fonksiyon.java
+++ src/Fonksiyon.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Fonksiyon {
     public void Fonksiyon(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Sayi gir: ");
+        System.out.print("x gir: ");
         int x = scanner.nextInt();
-        System.out.print("Sayi gir: ");
+        System.out.print("y gir: ");
         int y = scanner.nextInt();
         int sonuc=0;
         if(x>0 && y<0)
         {
-            System.out.print("f(x,y)= ");
             sonuc = (4*x)+(2*y)+4;
+            System.out.print("f(x,y)= " + sonuc);
         }
         if(x>0 && y==0)
         {
-            System.out.print("f(x,y)= ");
             sonuc = (2*x)-y-3;
+            System.out.print("f(x,y)= "+sonuc);
         }
         if(x<0 && y>0)
         {
-            System.out.print("f(x,y)= ");
             sonuc = (3*x)+(4*y)+3;
+            System.out.print("f(x,y)= "+sonuc);
         }
     }
 }
