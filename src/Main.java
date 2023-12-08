542cab7f1939f7417d86dad6bdc1b6ae695392bd
zehrasbr <zehraisbr@gmail.com>
2023-12-08 12:50:22 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,8 @@ public class Main {
         //x > 0 , y < 0 ise f(x,y) = 4x +2y +4
         //x > 0 , y =0 ise f(x,y) = 2x - y - 3
         //x < 0 , y > 0 ise f(x,y) = 3x + 4y +3
-        Fonksiyon fonksiyon = new Fonksiyon();
-        fonksiyon.Fonksiyon();
+        //Fonksiyon fonksiyon = new Fonksiyon();
+        //fonksiyon.Fonksiyon();
     }
 }
 
