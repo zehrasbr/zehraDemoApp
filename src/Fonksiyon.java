b96f66958c1a9e63034ffa3e3f8abab6a876a2ae
zehrasbr <zehraisbr@gmail.com>
2023-12-08 18:02:08 +0300

    update

--- src/Fonksiyon.java
+++ src/Fonksiyon.java
@@ -8,17 +8,18 @@ public class Fonksiyon {
         System.out.print("y gir: ");
         int y = scanner.nextInt();
         int sonuc=0;
+
         if(x>0 && y<0)
         {
             sonuc = (4*x)+(2*y)+4;
             System.out.print("f(x,y)= " + sonuc);
         }
-        if(x>0 && y==0)
+        else if (x>0 && y==0)
         {
             sonuc = (2*x)-y-3;
             System.out.print("f(x,y)= "+sonuc);
         }
-        if(x<0 && y>0)
+        else if (x<0 && y>0)
         {
             sonuc = (3*x)+(4*y)+3;
             System.out.print("f(x,y)= "+sonuc);
